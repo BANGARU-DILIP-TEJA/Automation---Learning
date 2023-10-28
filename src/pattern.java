f5257541f31658a893acf8a485bb30bb4b09051e
BANGARU DILIP TEJA <79089892+BANGARU-DILIP-TEJA@users.noreply.github.com>
2023-10-28 11:38:54 +0100

    Update pattern.java

--- src/pattern.java
+++ src/pattern.java
@@ -22,9 +22,12 @@ public class pattern {
     }*/
     public static void main(String[]args)
     {
-        int a=7;
+       int a=5;
         for(int i=1;i<=a;i++)
         {
+            for(int j=1;j<=i;j++)
+                System.out.print("@");
+            System.out.println();
 
         }
     }
